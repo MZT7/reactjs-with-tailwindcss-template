4b232a689ec6fab4a5f22053ad92c5cb8425956a
Mezu <duzioristo@gmail.com>
2022-05-10 21:15:57 +0100

    update

--- resources/js/components/inc/Header.js
+++ resources/js/components/inc/Header.js
@@ -215,13 +215,12 @@ function Header() {
                     id="mobile-menu"
                 >
                     <div className=" px-2 pt-2 pb-3 space-y-1">
-                        <a
-                            href="#"
+                        <Link
+                            to="/"
                             className=" text-white block px-3 py-2 rounded-md text-base font-medium"
-                            aria-current="page"
                         >
-                            Dashboard
-                        </a>
+                            Home
+                        </Link>
 
                         <a
                             href="#"
